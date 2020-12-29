5e12c7b5b8e2856a552da36589dbf8e322d59f3e
Taylor Cooke <tcooke@relias.com>
2020-12-29 13:49:57 -0500

    code complete. working. hot

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@/plugins/echarts'
+import 'core-js'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
